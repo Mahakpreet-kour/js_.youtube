cd139e083c690325673e93d4ffd787ea3eecaf9c
Mahakpreet-kour <mahakmakkar621@gmail.com>
2024-10-04 17:32:02 +0000

    coomit

---
+++ .devcontainer/01_basicss/05_strings.js
@@ -0,0 +1,6 @@
+const name="Mahak"
+const rs=50
+//console.log(line + rs + " but i have to pay 60 rs to buy burger")
+
+console.log(`Hello my name is ${name} and i have ${rs} rupees`)
+//use backticks for string interposition
