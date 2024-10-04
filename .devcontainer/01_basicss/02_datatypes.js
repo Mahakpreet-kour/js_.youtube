c5fc9c5e7b3c854ab0eb56cb6357f39ae1b2981a
Mahakpreet-kour <mahakmakkar621@gmail.com>
2024-10-04 13:49:47 +0000

    commit these

---
+++ .devcontainer/01_basicss/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"  //use all JS code as newer version
+
+//alert(3+3)   //we are using nodejs not browser
+
+console.log(3+9)
+console.log("Mahak")  //code must be readable.... cannot be like console.log(); console.log("Mahak") 
+// not in same line....  //code readibility must be high
+
+let name="Mahak"
+let age=12
+let isLoggedin= true;
+
+//num --> 2 to the power 53
+//bigint
+//string => ""
+//boolean=> true/false
+//null=> standalone value //representation of empty value
+//undefined
+//symbol =>unique
+
+//object
+
+console.log(typeof"Mahak")
+console.log(typeof age)
+console.log(typeof null)  //output is obj...null is a object
+console.log(typeof undefined) //output is undefined
---
+++ .devcontainer/01_basicss/03_conversionOperation.js
@@ -0,0 +1,79 @@
+let score= true
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber= Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//"33"=>33
+//"33abc"=>NaN
+//true=> 1
+
+let isLoggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(typeof booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1=>true;  0=>false
+//""=>false
+//"Mhak"=>true
+
+let someNumber=33
+let stringnumber= String(someNumber)
+console.log(typeof stringnumber)
+
+/*******************Operations******************/
+let value=3
+let negvalue=-value
+console.log(negvalue)
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2**2);
+//console.log(2%3);
+
+let str1="hello"
+let str2=" Mahak"
+//console.log(str1 + str2)
+//console.log("1"+2)
+//console.log(1+"2");
+//console.log("1"+2+2);
+//console.log(1+2+"2")
+//console.log((2+3)*5/6)
+
+//console.log(true)
+//console.log(+true)
+//console.log(true+1)
+//console.log(+"")
+
+console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2!=0)
+//console.log("2">1)
+//console.log("02">1)
+
+//console.log(null==0)
+//console.log(null>0)
+//console.log(null<=0)
+//console.log(undefined==0)  //equality and comparison operators works differentlyy
+//avoid these above and lower coversions as these confuses youuu
+//console.log(undefined>0)
+//console.log(undefined>=0)
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+
+
+
+
+let gamecounter=100
+gamecounter++
+console.log(gamecounter)
+
+//===  strict check,it also checks the data type with value
+console.log('2'===2)
